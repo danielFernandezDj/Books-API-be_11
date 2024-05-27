135d1510a7e08d199d7219f8db4beffe2175337d
danielFernandezJd <mdanif9728@gmail.com>
2024-05-27 15:34:24 -0700

    GET a single book by ID it working

--- controllers/books_controller.js
+++ controllers/books_controller.js
@@ -3,51 +3,6 @@ const express = require('express');
 const books = express.Router();
 const Books = require('../models/books');
 
-// Seeds db
-// books.get('/seed', (req, res) => {
-//     Books.insertMany([
-//         {
-//             "title": "The Shin Initiative",
-//             "description": "The reality-bending adventures of a clandestine service agency in the year 2166",
-//             "year": 2014,
-//             "quantity": 10,
-//             "imageURL": "https://imgur.com/LEqsHy5.jpeg"
-//         },
-//         {
-//             "title": "Tess the Wonder Dog",
-//             "description": "The tale of a dog who gets super powers",
-//             "year": 2007,
-//             "quantity": 3,
-//             "imageURL": "https://imgur.com/cEJmGKV.jpg"
-//         },
-//         {
-//             "title": "The Annals of Art",
-//             "description": "This anthology tells the intertwined narratives of six fairy tales.",
-//             "year": 2016,
-//             "quantity": 8,
-//             "imageURL": "https://imgur.com/VGyUtrr.jpeg"
-//         },
-//         {
-//             "title": "Wrap",
-//             "description": "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
-//             "year": 2010,
-//             "quantity": 4,
-//             "imageURL": "https://imgur.com/qYLKtPH.jpeg"
-//         }
-//     ].map(book => ({ ...book, _id: undefined })))
-//         .then(createBooks => {
-//             res.json({ message: "Seed successful!" });
-//         })
-//         .catch(error => {
-//             console.error('Error seeding books:', error);
-//             res.status(500).json({ error: 'Failed to seed books' });
-//         });
-// })
-
-
-
-
-
 // GET all books 
 books.get('/', (req, res) => {
     Books.find()
@@ -61,8 +16,8 @@ books.get('/', (req, res) => {
 });
 
 // GET a single book by ID
-books.get('/:title', (req, res) => {
-    books.findById(req.params.id)
+books.get('/:id', (req, res) => {
+    Books.findById(req.params.id)
         .then(foundBooks => {
             if (foundBooks) {
                 res.json(foundBooks);
